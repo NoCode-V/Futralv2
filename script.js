2a4118c964d050c2cc4dacc8a9e07bf16139b66c
NoCode-V <cichy.tbn@gmail.com>
2025-03-28 20:36:31 +0100

    Update script.js
    Update 0.3 - band photos, gallery tweak

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const imageUrls = [
-  "https://plus.unsplash.com/premium_photo-1682855223699-edb85ffa57b3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1445375011782-2384686778a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmFuZHxlbnwwfHwwfHx8MA%3D%3D",
-  "https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?q=80&w=2716&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1605340406960-f5b496c38b3d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://plus.unsplash.com/premium_photo-1682600420019-bf44716aa917?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://plus.unsplash.com/premium_photo-1682855223699-edb85ffa57b3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1445375011782-2384686778a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmFuZHxlbnwwfHwwfHx8MA%3D%3D",
-  "https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?q=80&w=2716&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1605340406960-f5b496c38b3d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://plus.unsplash.com/premium_photo-1682600420019-bf44716aa917?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  "photos/foto1.jpg",
+  "photos/foto2.jpg",
+  "photos/foto3.jpg",
+  "photos/foto4.jpg",
+  "photos/foto5.jpg",
+  "photos/foto6.jpg",
+  "photos/foto7.jpg",
+  "photos/foto8.jpg",
+  "photos/foto9.jpg",
+  "photos/foto10.jpg",
+  "photos/foto11.jpg",
+  "photos/foto12.jpg"
 ];
 
 const imagesContainer = document.querySelector('.gallery-images');
@@ -35,6 +37,5 @@ function changeSlide(direction) {
   }
 }
 
-// Initialize the gallery display
 loadImages();
 changeSlide(0);
